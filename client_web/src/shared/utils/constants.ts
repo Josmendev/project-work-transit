57d53ca9373a284258cd966192c52cf3cc478c8f
CTZDev <carlosteranzavaleta@hotmail.com>
2025-04-24 21:01:12 -0500

    feat(constants): add initial constants and routes for application structure

---
+++ client_web/src/shared/utils/constants.ts
@@ -0,0 +1,70 @@
+export const BASE_BACKEND_URL = "http://localhost:3002/api/v1";
+
+export const BASE_ROUTES = {
+  PUBLIC: {
+    LOGIN: "/login",
+    CONFIRM_ACCOUNT: "confirm-account",
+    HOME: "/",
+  },
+  PRIVATE: {
+    DASHBOARD: "dashboard",
+    ADMIN: "admin",
+    VEHICLES: "vehicles",
+    PATHS: "paths",
+    TRIPS: "trips",
+  },
+};
+
+export const CRUD_ROUTES = {
+  LIST: "list",
+  ADD: "add",
+  DETAIL: ":id",
+  EDIT: ":id/edit",
+};
+
+// Asiginacion del router en los modulos
+export const ADMIN_USERS_ROUTES = {
+  USERS: "users",
+  ROLES: "roles",
+  STAFF: "staff",
+};
+
+export const VEHICLES_ROUTES = {
+  BRAND: "brand",
+  MODEL: "model",
+  VEHICLE: "vehicle",
+};
+
+export const PATH_ROUTES = {
+  PATH: "path",
+  CONNECTION_POINTS: "connection-points",
+  ZONE: "zone",
+  BUS_STOP: "bus-stop",
+};
+
+export const TRIPS_ROUTES = {
+  TRIP: "trip",
+};
+
+// Defino un mapa de roles
+export const ROLES_MAPPING: Record<string, number> = {
+  Administrador: 1,
+  Pasajero: 2,
+  Chofer: 3,
+} as const;
+
+// Defino un tipo con las claves (Administrador, Chofer, Pasajero) y tipo con valores con 1,2 y 3
+export type ROLES_KEYS = keyof typeof ROLES_MAPPING;
+export type ROLES_VALUES = (typeof ROLES_MAPPING)[ROLES_KEYS];
+
+export const INITIAL_PAGE = 1;
+export const LIMIT_PAGE = 5;
+
+// Defino el estado inicial para todos los listados de tablas
+export const initialListOfResponseAPI = {
+  data: [],
+  limit: 5,
+  page: 1,
+  total: 1,
+  totalPages: 1,
+};
