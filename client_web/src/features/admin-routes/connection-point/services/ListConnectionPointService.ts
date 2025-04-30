a819c0c3212b96899ea003fd46951fb0ff88dfe9
CTZDev <carlosteranzavaleta@hotmail.com>
2025-04-30 16:30:04 -0500

    feat(connection-points): implement ListConnectionPointService for fetching connection points

---
+++ client_web/src/features/admin-routes/connection-point/services/ListConnectionPointService.ts
@@ -0,0 +1,37 @@
+import type { DataResponseFromAPI } from "../../../../shared/types/DataResponse";
+import { INITIAL_PAGE, LIMIT_PAGE } from "../../../../shared/utils/constants";
+import { handleApiError } from "../../../../shared/utils/handleApiError";
+import type { ConnectionPointResponse } from "../types/ConnectionPoint";
+import { ENDPOINT_CONNECTION_POINT } from "../utils/endpoints";
+
+// Creo la funcion que se conecta a la API del backend
+export const ListConnectionPointService = async ({
+  limit = LIMIT_PAGE,
+  page = INITIAL_PAGE,
+}: {
+  limit?: number;
+  page: number;
+}): Promise<DataResponseFromAPI<ConnectionPointResponse>> => {
+  try {
+    const { token } = JSON.parse(sessionStorage.getItem("user") as string);
+    if (!token) throw new Error("Token inválido");
+
+    const response = await fetch(`${ENDPOINT_CONNECTION_POINT}?limit=${limit}&page=${page}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+
+    // Respuesta no exitosa
+    if (!response.ok) throw await response.json();
+
+    // Respuesta exitosa
+    const data: DataResponseFromAPI<ConnectionPointResponse> = await response.json();
+    return data;
+  } catch (error: unknown) {
+    handleApiError(error);
+    return Promise.reject(error);
+  }
+};
