e67fb3abf65d2bcaed350edea0fb6d4753a845b8
CTZDev <carlosteranzavaleta@hotmail.com>
2025-04-30 16:31:49 -0500

    feat(routes): add CreateRouteService for creating routes via API

---
+++ client_web/src/features/admin-routes/route/services/CreateRouteService.ts
@@ -0,0 +1,34 @@
+import { handleApiError } from "../../../../shared/utils/handleApiError";
+import type { RouteResponse, UpsertRoute } from "../types/Route";
+import { ENDPOINT_ROUTE } from "../utils/endpoints";
+
+// Creo la funcion que se conecta a la API del backend
+export const CreateRouteService = async ({
+  route,
+}: {
+  route: UpsertRoute;
+}): Promise<RouteResponse> => {
+  try {
+    const { token } = JSON.parse(sessionStorage.getItem("user") as string);
+    if (!token) throw new Error("Token inválido");
+
+    const response = await fetch(`${ENDPOINT_ROUTE}`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+      body: JSON.stringify(route),
+    });
+
+    // Respuesta no exitosa
+    if (!response.ok) throw await response.json();
+
+    // Respuesta exitosa
+    const data: RouteResponse = await response.json();
+    return data;
+  } catch (error: unknown) {
+    handleApiError(error);
+    return Promise.reject(error);
+  }
+};
