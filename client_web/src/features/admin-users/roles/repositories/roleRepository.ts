97c8f77243fe802cff513e1f767f422eb3e3ca4e
CTZDev <carlosteranzavaleta@hotmail.com>
2025-04-30 16:37:43 -0500

    fix(roleRepository): update role types to CreateRole and UpdateRole for consistency

--- client_web/src/features/admin-users/roles/repositories/roleRepository.ts
+++ client_web/src/features/admin-users/roles/repositories/roleRepository.ts
@@ -4,15 +4,15 @@ import { DeleteRoleService } from "../services/DeleteRoleService";
 import { ListRolesService } from "../services/ListRolesService";
 import { SearchRolesService } from "../services/SearchRolesService";
 import { UpdateRoleService } from "../services/UpdateRoleService";
-import type { UpsertRole } from "../types/Role";
+import type { CreateRole, UpdateRole } from "../types/Role";
 
-//Funcion para agregar rol
-export const createRole = async ({ role }: { role: UpsertRole }) => {
+//Funcion para agregar
+export const createRole = async ({ role }: { role: CreateRole }) => {
   const newRole = await CreateRoleService({ role });
   return newRole;
 };
 
-// Funcion para obtener y buscar todos los roles
+// Funcion para obtener y buscar
 export const getRoles = async ({
   limit,
   page,
@@ -25,18 +25,18 @@ export const getRoles = async ({
   return query ? SearchRolesService({ limit, page, query }) : ListRolesService({ limit, page });
 };
 
-// Funcion para actualizar un rol
-export const updateRole = async ({ role, roleId }: { role: UpsertRole; roleId: number }) => {
+// Funcion para actualizar
+export const updateRole = async ({ role, roleId }: { role: UpdateRole; roleId: number }) => {
   const updatedRole = await UpdateRoleService({ role, roleId });
   return updatedRole;
 };
 
-// Funcion para eliminar un rol
+// Funcion para eliminar
 export const deleteRole = async ({ roleId }: { roleId: number }) => {
   await DeleteRoleService({ roleId });
 };
 
-// Funcion para activar un rol
+// Funcion para activar
 export const activateRole = async ({ roleId }: { roleId: number }) => {
   await ActivateRoleService({ roleId });
 };
