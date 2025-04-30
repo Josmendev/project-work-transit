bcd7863349f6fdabbe150e8fbcc6e4afe69bd741
CTZDev <carlosteranzavaleta@hotmail.com>
2025-04-30 16:47:47 -0500

    refactor(DashBoardPage): update welcome message to reflect user information

--- client_web/src/shared/pages/DashBoardPage.tsx
+++ client_web/src/shared/pages/DashBoardPage.tsx
@@ -1,20 +1,20 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { getUserInformation } from "../helpers/getUserInformation";
 import DefaultLayout from "../layouts/DefaultLayout";
 import { SectionLayout } from "../layouts/SectionLayout";
 
 export const DashBoardPage: React.FC = () => {
   const { user } = useContext(AuthContext);
-
   if (!user) throw new Error("User is not defined");
 
-  const { userInformation } = getUserInformation(user);
+  // const { userInformation } = getUserInformation(user);
 
   return (
     <DefaultLayout>
       <SectionLayout title="Inicio" subtitle="Bienvenido">
-        <h3 className="text-h3-semibold">Bienvenido {userInformation ?? ""} 👋👋👋</h3>
+        <h3 className="text-h3-semibold">
+          Bienvenido {"userInformation - ACTUALIZAR DSPS"} 👋👋👋
+        </h3>
       </SectionLayout>
     </DefaultLayout>
   );
