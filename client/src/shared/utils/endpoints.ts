5bb8ec20c634698f2a8ef2cbbe6e1f2a4603a015
CTZDev <carlosteranzavaleta@hotmail.com>
2025-05-03 21:55:54 -0500

    fix(endpoints): ensure consistency in endpoint URLs for authentication

--- client/src/shared/utils/endpoints.ts
+++ client/src/shared/utils/endpoints.ts
@@ -4,5 +4,8 @@ export const ENDPOINTS_AUTH = {
   LOGIN: `${BASE_BACKEND_URL}/auth/login`,
   CONFIRM_ACCOUNT: `${BASE_BACKEND_URL}/auth/confirm-account`,
   PROFILE: `${BASE_BACKEND_URL}/auth/user-profile`,
+  REQUEST_RESET_PASSWORD: `${BASE_BACKEND_URL}/auth/request-password-reset`,
+  VALIDATION_PIN: `${BASE_BACKEND_URL}/auth/validation-pin`,
+  CHANGE_PASSWORD: `${BASE_BACKEND_URL}/auth/change-password`,
   LOGOUT: `${BASE_BACKEND_URL}/auth/logout`,
 } as const;
