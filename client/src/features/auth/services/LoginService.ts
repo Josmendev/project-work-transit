bef736fae8e54093d67e6c93e6b54355a32bb8c1
CTZDev <carlosteranzavaleta@hotmail.com>
2025-05-03 21:54:13 -0500

    feat(auth): implement LoginService for user authentication

--- client/src/features/auth/services/LoginService.ts
+++ client/src/features/auth/services/LoginService.ts
@@ -1,26 +1,30 @@
 import { ENDPOINTS_AUTH } from "../../../shared/utils/endpoints";
 import { handleApiError } from "../../../shared/utils/handleApiError";
-import { type AuthUserLogin, type AuthUserResponse } from "../types/authTypes";
+import { type AuthUserLogin, type AuthUserResponse } from "../types/Auth";
 
 // Creo la funcion login que se conecta a la API del backend
-export const LoginService = async (user: AuthUserLogin): Promise<AuthUserResponse> => {
+export const LoginService = async ({
+  user,
+}: {
+  user: AuthUserLogin;
+}): Promise<AuthUserResponse> => {
   try {
     const response = await fetch(`${ENDPOINTS_AUTH.LOGIN}`, {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify({ ...user }),
     });
 
-    // Respuesta no exitosa, lanzo excepcion del backend
+    // Respuesta no exitosa
     if (!response.ok) throw await response.json();
 
-    // Respuesta exitosa, parseo el JSON y devuelvo el objeto AuthResponseUser
+    // Respuesta exitosa
     const data: AuthUserResponse = await response.json();
     return data;
   } catch (error: unknown) {
     handleApiError(error);
-    return Promise.reject(error); // Similar al throw, pero enfocado a promesas
+    return Promise.reject(error);
   }
 };
